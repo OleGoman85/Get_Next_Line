c70f21aa11acc8b7eef565205d0d89f7c59b52df
Oleg Goman <olegoman85@gmail.com>
2023-11-27 15:53:31 +0200

    update GNL

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ogoman <ogoman@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 07:31:34 by ogoman            #+#    #+#             */
-/*   Updated: 2023/11/24 15:13:12 by ogoman           ###   ########.fr       */
+/*   Updated: 2023/11/27 15:51:25 by ogoman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,19 @@
 
 # endif //BUFFER_SIZE 
 
-# include <fcntl.h>
-# include <stdio.h>
+// # include <fcntl.h> // for open in main (del)
+// # include <stdio.h> // for printf in main (del)
 # include <stdlib.h>
 # include <unistd.h>
+// # include <stddef.h> // ?
+// # include <stdio.h> // main (del)
 
-static void	copy_s2_in_s1(char *s1, char *s2);
-char		*get_next_line(int fd);
-static int	ft_find_line(char *buf, char **line);
-static char	*free_line(char **line);
-char		*ft_strjoin(char *s1, char *s2);
-size_t		ft_strlen(const char *s);
-void		*ft_calloc(size_t count, size_t size);
+char	*get_next_line(int fd);
+void	*custom_memchr(const void *buffer, void *start, int tar, size_t size);
+void	*custom_memcpy(void *destination, const void *source, size_t num_bytes);
+char	*join_str(void *dest, void const *source, size_t dest_l, size_t src_l);
+void	save_buffer(char *buf_save, ssize_t buf_l);
+char	*read_line(int fd, char *line, void *buf, ssize_t *bytes_read);
+size_t	ft_strlen(const char *str);
 
-#endif // GET_NEXT_LINE-_H
+#endif // GET_NEXT_LINE_H;
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,68 +6,182 @@
 /*   By: ogoman <ogoman@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 07:31:16 by ogoman            #+#    #+#             */
-/*   Updated: 2023/11/24 14:31:32 by ogoman           ###   ########.fr       */
+/*   Updated: 2023/11/27 15:49:37 by ogoman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_strjoin(char *s1, char *s2)
+char	*join_str(void *dest, void const *source, size_t dest_l, size_t src_l)
 {
-	char	*new;
-	size_t	i;
+	char	*res_str;
 
-	if (!s1 || !s2)
-		return (NULL);
-	new = (char *)ft_calloc(sizeof(char), (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (!new)
-		return (NULL);
-	i = 0;
-	while (*s1)
+	res_str = (char *)malloc((dest_l + src_l + 1) * sizeof(char));
+	if (res_str != NULL)
 	{
-		new[i] = *s1;
-		i++;
-		s1++;
+		custom_memcpy(res_str, dest, dest_l);
+		custom_memcpy(res_str + dest_l, source, src_l);
+		res_str[dest_l + src_l] = '\0';
 	}
-	while (*s2)
-	{
-		new[i] = *s2;
-		i++;
-		s2++;
-	}
-	new[i] = '\0';
-	return (new);
+	free(dest);
+	return (res_str);
 }
 
-size_t	ft_strlen(const char *s)
+void	*custom_memchr(const void *buffer, void *start, int targ, size_t size)
 {
-	size_t	length;
+	size_t	index;
 
-	length = 0;
-	while (s[length] != '\0')
+	index = 0;
+	while (index < size)
 	{
-		length++;
+		if (((unsigned char *)buffer)[index] == (unsigned char)targ)
+			return ((void *)buffer + index);
+		index++;
 	}
-	return (length);
+	return (start);
 }
 
-void	*ft_calloc(size_t count, size_t size)
+void	*custom_memcpy(void *destination, const void *source, size_t num_bytes)
 {
-	size_t			total_size;
-	size_t			i;
-	void			*ptr;
-	unsigned char	*byte_ptr;
+	size_t	index;
 
-	total_size = count * size;
-	ptr = malloc(total_size);
-	if (!ptr)
+	index = 0;
+	if (destination == NULL && source == NULL)
 		return (NULL);
-	byte_ptr = (unsigned char *)ptr;
-	i = 0;
-	while (i < total_size)
+	while (index < num_bytes)
 	{
-		byte_ptr[i] = 0;
-		i++;
+		((char *)destination)[index] = ((char *)source)[index];
+		index++;
 	}
-	return (ptr);
+	return (destination);
 }
+
+// char	*ft_strjoin(char *s1, char *s2)
+// {
+// 	char	*new;
+// 	size_t	i;
+
+// 	if (!s1 || !s2)
+// 		return (NULL);
+// 	new = (char *)ft_calloc(sizeof(char), (ft_strlen(s1) + ft_strlen(s2) + 1));
+// 	if (!new)
+// 		return (NULL);
+// 	i = 0;
+// 	while (*s1)
+// 	{
+// 		new[i] = *s1;
+// 		i++;
+// 		s1++;
+// 	}
+// 	while (*s2)
+// 	{
+// 		new[i] = *s2;
+// 		i++;
+// 		s2++;
+// 	}
+// 	new[i] = '\0';
+// 	return (new);
+// }
+
+// size_t	ft_strlen(const char *s)
+// {
+// 	size_t	length;
+
+// 	length = 0;
+// 	while (s[length] != '\0')
+// 	{
+// 		length++;
+// 	}
+// 	return (length);
+// }
+
+// void	*ft_calloc(size_t count, size_t size)
+// {
+// 	size_t			total_size;
+// 	size_t			i;
+// 	void			*ptr;
+// 	unsigned char	*byte_ptr;
+
+// 	total_size = count * size;
+// 	ptr = malloc(total_size);
+// 	if (!ptr)
+// 		return (NULL);
+// 	byte_ptr = (unsigned char *)ptr;
+// 	i = 0;
+// 	while (i < total_size)
+// 	{
+// 		byte_ptr[i] = 0;
+// 		i++;
+// 	}
+// 	return (ptr);
+// }
+
+///////////
+
+// size_t	ft_strlen(const char *s)
+// {
+// 	size_t	length;
+
+// 	length = 0;
+// 	while (*s)
+// 	{
+// 		s++;
+// 		length++;
+// 	}
+// 	return (length);
+// }
+
+// void	*ft_memset(void *b, int c, size_t len)
+// {
+// 	unsigned char	*val;
+// 	int				i;
+
+// 	i = 0;
+// 	val = (unsigned char *)b;
+// 	while (len > 0)
+// 	{
+// 		val[i++] = (unsigned char)c;
+// 		len--;
+// 	}
+// 	return (b);
+// }
+
+// void	*ft_calloc(size_t count, size_t size)
+// {
+// 	void	*mem;
+// 	int		i;
+
+// 	i = 0;
+// 	mem = malloc(count * size);
+// 	if (mem == NULL)
+// 		return (NULL);
+// 	ft_memset(mem, '\0', (count * size));
+// 	return (mem);
+// }
+
+// char	*ft_strjoin(char *s1, char *s2)
+// {
+// 	char	*new;
+// 	size_t	i;
+
+// 	if (!s1 || !s2)
+// 		return (NULL);
+// 	new = (char *)ft_calloc(sizeof(char), (ft_strlen (s1) + ft_strlen(s2) + 1));
+// 	if (!new)
+// 		return (NULL);
+// 	i = 0;
+// 	while (*s1)
+// 	{
+// 		new[i] = *s1;
+// 		i++;
+// 		s1++;
+// 	}
+// 	while (*s2)
+// 	{
+// 		new[i] = *s2;
+// 		i++;
+// 		s2++;
+// 	}
+// 	new[i] = '\0';
+// 	return (new);
+// }
